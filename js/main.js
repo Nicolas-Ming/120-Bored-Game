bcec2391ba9efe17baf0661f0e6a03a5732a281a
Nicolas Ming <ncming@ucsc.edu>
2018-08-07 01:05:56 -0700

    added isometric for day not fully implemented, still figuring out floor tiles and movement

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 var game = new Phaser.Game(1024,1024);
 
 //global variables
-
+console.log('preload: main')
 
 //state management
 game.state.add('boot',boot);
+game.state.add('mainMenu',mainMenu);
+game.state.add('day',day);
+//game.state.add('night',night);
+//game.state.add('talking',talking);
+//game.state.add('settings',settings);
 game.state.start('boot');
