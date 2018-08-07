2f31f200794fbdea6dec4a1f5ab793f5feacbe37
Nic <ncming@ucsc.edu>
2018-08-07 15:27:55 -0700

    fixed tile for now

---
+++ js/mainMenu.js
@@ -0,0 +1,15 @@
+var mainMenu = function() {
+	//local variables
+};
+mainMenu.prototype =  {
+	preload: function() {
+		console.log('preload: MM');
+		game.state.start('day');
+	},
+	create: function() {
+
+	},
+	update: function() {
+
+	},
+}
