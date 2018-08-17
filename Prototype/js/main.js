c9814f6fe084b6d40f3456b7821a10cfd3a60a11
warnerScheibe <42053374+warnerScheibe@users.noreply.github.com>
2018-08-16 17:40:01 -0700

    Add files via upload
    movementsssssss

--- Prototype/js/main.js
+++ Prototype/js/main.js
@@ -1,23 +1,15 @@
-var game = new Phaser.Game();
+//"use strict";
 
-/*
-cactusboi = game.load.image('cactusboi', 'assets/img/cactusboi/cactusboi.png');
-game.load.image('lefthand', 'assets/img/cactusboi/lefthand.png');
-game.load.image('righthand', 'assets/img/cactusboi/righthand.png');
-game.load.image('jacket', 'assets/img/cactusboi/jacket.png');
-game.load.image('hat', 'assets/img/cactusboi/hat.png');
-game.load.image('cactusnoface', 'assets/img/cactusboi/cactusnoface.png');
+var game = new Phaser.Game(1080, 720, Phaser.AUTO);
 
-game.load.image('pupperfull', 'assets/img/pupper/pupperfull.png');
-game.load.image('justdog', 'assets/img/pupper/justdog.png');
-game.load.image('bodyscarf', 'assets/img/pupper/bodyscarf.png');
-game.load.image('headscarf', 'assets/img/pupper/headscarf.png');
-
-game.load.image('button', 'assets/img/buttons/button.png');
-game.load.image('eye', 'assets/img/buttons/eye.png');
-*/
-
+var counter = 0;
+var cactusEndX = 260;
+var cactusEndY = 140;
+var startCactusNoFaceX = 860;
+var startCactusNoFaceY = 475;
+//game.state.add('Dialogue', Dialogue);
 game.state.add('Button', Button);
+//game.state.add('cactusBuilder', cactusBuilder);
 //game.state.add('objTweening', objTweening);
 //game.state.start('objTweening');
 game.state.start('Button');
